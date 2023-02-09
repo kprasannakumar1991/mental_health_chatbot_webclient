af5cfcd53b43a9e85300de0be70d8d23fd8523f1
K Prasanna Kumar <kprasannakumar1991@gmail.com>
2023-02-09 18:37:41 +0530

    backend deployed in AWS

--- src/api/chatbot.js
+++ src/api/chatbot.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://mental-health-chatbot-backend.herokuapp.com/',
+    baseURL: 'http://mentalhealthchatbot-env.eba-ep2rbvgh.ap-south-1.elasticbeanstalk.com/',
+    // baseURL: 'https://mental-health-chatbot-backend.herokuapp.com/',
     // baseURL: 'http://localhost:5000/',
     headers: {
         'Content-Type': 'application/json'
