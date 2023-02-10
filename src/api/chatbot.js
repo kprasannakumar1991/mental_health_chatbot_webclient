10042a2853ace0e1614155145b57439f49b8be80
K Prasanna Kumar <kprasannakumar1991@gmail.com>
2023-02-10 16:11:31 +0530

    Modified URL

--- src/api/chatbot.js
+++ src/api/chatbot.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://mentalhealthchatbot-env.eba-ep2rbvgh.ap-south-1.elasticbeanstalk.com/',
-    // baseURL: 'https://mental-health-chatbot-backend.herokuapp.com/',
+    baseURL: 'https://prasannabackend.com/',
     // baseURL: 'http://localhost:5000/',
     headers: {
         'Content-Type': 'application/json'
